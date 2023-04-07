74b143996af4c8c3d01f2e89fc0551847f0661bd
Yucheng Lu <yl5163@columbia.edu>
2023-04-07 15:16:28 -0400

    finished and worked through the given test

--- src/minishell.c
+++ src/minishell.c
@@ -5,12 +5,16 @@
 #include <sys/wait.h>
 #include <signal.h>
 #include <pwd.h>
+#include <stdbool.h>
+#include <errno.h>
 
 #define BRIGHTBLUE "\x1b[34;1m"
 #define DEFAULT    "\x1b[0m"
 
 #define MAX_INPUT_SIZE 1024
 #define MAX_PATH_SIZE 1024
+#define MAX_INPUT_LEN 4096
+#define MAX_NUM_TOKENS 2048
 
 void run_shell();
 
@@ -29,6 +33,9 @@ char *expand_tilde(const char *path) {
             strcpy(new_path, pw->pw_dir);
             strcat(new_path, path + 1);
             return new_path;
+        } else {
+            fprintf(stderr, "Error: Cannot get passwd entry. %s.\n", strerror(errno));
+            exit(EXIT_FAILURE);
         }
     }
     return strdup(path);
@@ -38,11 +45,12 @@ void change_directory(char *path) {
     char *expanded_path = expand_tilde(path);
 
     if (chdir(expanded_path) != 0) {
-        perror("chdir");
+        fprintf(stderr, "Error: Cannot change directory to '%s'. %s.\n", expanded_path, strerror(errno));
     }
 
     free(expanded_path);
 }
+
 char *get_next_token(char *input, int *pos) {
     int start = *pos;
     char *token;
@@ -74,6 +82,10 @@ char *get_next_token(char *input, int *pos) {
 
     if (token_len > 0) {
         token = malloc(token_len + 1);
+        if (token == NULL) {
+            fprintf(stderr, "Error: malloc() failed. %s.\n", strerror(errno));
+            exit(EXIT_FAILURE);
+        }
         int token_pos = 0;
         for (int i = start; i < *pos; i++) {
             if (input[i] != '\"') {
@@ -86,112 +98,99 @@ char *get_next_token(char *input, int *pos) {
     }
 
     return token;
-} 
-/*
-char *get_next_token(char *input, int *pos) {
-    int start = *pos;
-    char *token;
-    int token_len = 0;
-    int in_quotes = 0;
-
-    while (input[*pos] != '\0') {
-        if (input[*pos] == '\"') {
-            in_quotes = !in_quotes;
-            (*pos)++;
-        } else {
-            if (input[*pos] == ' ' && !in_quotes) {
-                break;
-            }
-            token_len++;
-            (*pos)++;
-        }
-    }
-
-    if (token_len > 0) {
-        token = malloc(token_len + 1);
-        int token_pos = 0;
-        for (int i = start; i < start + token_len + (in_quotes ? 1 : 0); i++) {
-            if (input[i] != '\"') {
-                token[token_pos++] = input[i];
-            }
-        }
-        token[token_len] = '\0';
-    } else {
-        token = NULL;
-    }
-
-    if (input[*pos] == ' ') {
-        (*pos)++;
-    }
-    printf("Token: '%s'\n", token);
-    return token;
 }
-*/
+
+bool check_quotes_balance(const char *input) {
+    int quotes = 0;
+    for (int i = 0; input[i] != '\0'; i++) {
+        if (input[i] == '\"') {
+            quotes++;
+        }
+    }
+    return quotes % 2 == 0;
+}
+
 void run_shell() {
-    char input[MAX_INPUT_SIZE];
+    char input[MAX_INPUT_LEN];
     char cwd[MAX_PATH_SIZE];
 
     while (1) {
+	if (signal(SIGINT, sigint_handler) == SIG_ERR) {
+        	fprintf(stderr, "Error: Cannot register signal handler. %s.\n", strerror(errno));
+        	exit(EXIT_FAILURE);
+    	}
         if (getcwd(cwd, sizeof(cwd)) != NULL) {
-            printf("%s[%s]%s$ ", BRIGHTBLUE, cwd, DEFAULT);
+            	printf("%s[%s]%s$ ", BRIGHTBLUE, cwd, DEFAULT);
         } else {
-            perror("getcwd");
-            exit(EXIT_FAILURE);
-        }
+            	fprintf(stderr, "Error: Cannot get current working directory. %s.\n", strerror(errno));
+	    	exit(EXIT_FAILURE);
+	}
+	if (fgets(input, MAX_INPUT_SIZE, stdin) == NULL) {
+        	fprintf(stderr, "Error: Failed to read from stdin. %s.\n", strerror(errno));
+        	exit(EXIT_SUCCESS);
+    	}
 
-        if (fgets(input, MAX_INPUT_SIZE, stdin) == NULL) {
-            printf("\n");
-            exit(EXIT_SUCCESS);
-        }
+    	input[strcspn(input, "\n")] = 0;
 
-        input[strcspn(input, "\n")] = 0;
+    	if (!check_quotes_balance(input)) {
+        	printf("Error: Unbalanced quotes, there must be an even number of quotes.\n");
+        	printf("Exiting...\n");
+        	exit(EXIT_FAILURE);
+    	}
 
-        int pos = 0;
-        char *command = get_next_token(input, &pos);
-	printf("Input command: '%s'\n", command);
-        if (command == NULL) {
-            continue;
-        } else if (strcmp(command, "exit") == 0) {
-            free(command);
-            exit(EXIT_SUCCESS);
-        } else if (strcmp(command, "cd") == 0) {
-            char *path = get_next_token(input, &pos);
-            printf("Path is: '%s'\n", path);
-            if (path == NULL) {
-                change_directory("~");
-            } else {
-                change_directory(path);
-                free(path);
-            }
-        } else {
-            pid_t pid = fork();
-            if (pid < 0) {
-                perror("fork");
-                exit(EXIT_FAILURE);
-            } else if (pid == 0) {
-                char *args[MAX_INPUT_SIZE] = {command};
-                int i = 1;
-                char *arg;
-                while ((arg = get_next_token(input, &pos)) != NULL) {
-                    args[i++] = arg;
-                }
-  	        args[i] = NULL;	
-                if (execvp(command, args) == -1) {
-                    perror("execvp");
-                    exit(EXIT_FAILURE);
-                }
-            } else {
-                int status;
-                waitpid(pid, &status, 0);
-            }
-        }
-
-        free(command);
-    }
-}
+    	int pos = 0;
+    	char *command = get_next_token(input, &pos);
+    	if (command == NULL) {
+        	continue;
+    	} else if (strcmp(command, "exit") == 0) {
+        	free(command);
+        	exit(EXIT_SUCCESS);
+    	} else if (strcmp(command, "cd") == 0) {
+        	char *path = get_next_token(input, &pos);
+        	if (path == NULL) {
+        	    	change_directory("~");
+        	} else {
+            		change_directory(path);
+            		free(path);
+        	}
+    	} else {
+        	pid_t pid = fork();
+        	if (pid < 0) {
+            		fprintf(stderr, "Error: fork() failed. %s.\n", strerror(errno));
+            		exit(EXIT_FAILURE);
+        	} else if (pid == 0) {
+        	    	if (signal(SIGINT, SIG_DFL) == SIG_ERR) {
+        			fprintf(stderr, "Error: Cannot reset signal handler. %s.\n", strerror(errno));
+        			exit(EXIT_FAILURE);
+    			}	
+			char *args[MAX_INPUT_SIZE] = {command};
+            		int i = 1;
+            		char *arg;
+            		int num_tokens = 0;
+            		while ((arg = get_next_token(input, &pos)) != NULL) {
+                		args[i++] = arg;
+                		num_tokens++;
+            		}
+            		if (num_tokens > MAX_NUM_TOKENS) {
+                		printf("Error: Too many tokens in the input. Maximum allowed tokens: %d\n", MAX_NUM_TOKENS);
+                		exit(EXIT_FAILURE);
+            		}
+            		args[i] = NULL;
+            		if (execvp(command, args) == -1) {
+                		fprintf(stderr, "Error: exec() failed. %s.\n", strerror(errno));
+                		exit(EXIT_FAILURE);
+            		}
+        	} else {
+            		int status;
+            		if (waitpid(pid, &status, 0) < 0) {
+                		fprintf(stderr, "Error: wait() failed. %s.\n", strerror(errno));
+            		}
+        	}
+    	}
+    	free(command);
+}}
 
 int main() {
-    signal(SIGINT, sigint_handler);
     run_shell();
     return 0;
 }
